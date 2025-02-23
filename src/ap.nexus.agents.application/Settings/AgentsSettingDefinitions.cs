07dd796ca2458f1099893ff5be6b98f128ae63e1
Michael Josiah <Michael.Josiah@mandg.com>
2025-02-23 10:23:58 +0000

    Added AgentSetting

--- src/ap.nexus.agents.application/Settings/AgentsSettingDefinitions.cs
+++ src/ap.nexus.agents.application/Settings/AgentsSettingDefinitions.cs
@@ -52,27 +52,7 @@ namespace ap.nexus.agents.application.Settings
             };
         }
 
-        // Methods to access settings
-        //public async Task<int> GetBatchSizeAsync(Guid? tenantId = null)
-        //{
-        //    return await _settingManager.GetSettingValueAsync<int>(
-        //        BatchSize,
-        //        tenantId);
-        //}
-
-        //public async Task<bool> IsOcrEnabledAsync(Guid? tenantId = null)
-        //{
-        //    return await _settingManager.GetSettingValueAsync<bool>(
-        //        OcrEnabled,
-        //        tenantId);
-        //}
-
-        //public async Task<OcrConfig> GetOcrConfigAsync(Guid? tenantId = null)
-        //{
-        //    return await _settingManager.GetSettingValueAsync<OcrConfig>(
-        //        OcrConfig,
-        //        tenantId);
-        //}
+        
     }
 
     public class OcrConfig
---
+++ src/ap.nexus.agents.application/Settings/AgentsSettings.cs
@@ -0,0 +1,35 @@
+﻿using ap.nexus.abstractions.Frameworks.SettingManagement;
+
+namespace ap.nexus.agents.application.Settings
+{
+    public class AgentsSettings
+    {
+        private ISettingManager _settingManager;
+        public AgentsSettings(ISettingManager settingManager)
+        {
+            _settingManager = settingManager;
+        }
+
+        //Methods to access settings
+        public async Task<int> GetBatchSizeAsync(Guid? tenantId = null)
+        {
+            return await _settingManager.GetSettingValueAsync<int>(
+                AgentsSettingDefinitions.BatchSize,
+                tenantId);
+        }
+
+        public async Task<bool> IsOcrEnabledAsync(Guid? tenantId = null)
+        {
+            return await _settingManager.GetSettingValueAsync<bool>(
+                AgentsSettingDefinitions.OcrEnabled,
+                tenantId);
+        }
+
+        public async Task<OcrConfig> GetOcrConfigAsync(Guid? tenantId = null)
+        {
+            return await _settingManager.GetSettingValueAsync<OcrConfig>(
+                AgentsSettingDefinitions.OcrConfig,
+                tenantId);
+        }
+    }
+}
